c23e65be797c657b374eb03f6e08fabba0424484
mustafa fawzi <mfmj0512@gmail.com>
2019-07-27 00:13:20 +0300

    update 3

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-
-
+import 'package:email_validator/email_validator.dart';
 
 
 class Signup extends StatefulWidget {
@@ -13,7 +11,7 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
-
+  final _formKey = GlobalKey<FormState>();
   String message = "x";
 
   TextStyle style = TextStyle(
@@ -28,15 +26,15 @@ class _SignupState extends State<Signup> {
       if (response.statusCode == 201) {
         print("DONE!");
         setState(() {
-          message ="signup";
-          emailController.text ="";
-          nameController.text ="";
-          passController.text ="";
+          message = "signup";
+          emailController.text = "";
+          nameController.text = "";
+          passController.text = "";
         });
       } else {
         print("ERROR!");
         setState(() {
-          message ="error";
+          message = "error";
         });
       }
       print(response.body);
@@ -49,46 +47,50 @@ class _SignupState extends State<Signup> {
   final passController = TextEditingController();
   final nameController = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
-    final nameField = Padding(
-        padding: const EdgeInsets.only(top: 60.0, right: 15.0, left: 15.0),
-        child: TextField(
-          obscureText: false,
-          controller: nameController,
-          style: style,
-          textAlign: TextAlign.right,
-          keyboardType: TextInputType.text,
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
-            hintText: "اسم المستخدم",
-          ),
-        ));
-    final emailField = Padding(
-        padding: const EdgeInsets.only(right: 15.0, left: 15.0),
-        child: TextField(
-          obscureText: false,
-          controller: emailController,
-          style: style,
-          textAlign: TextAlign.right,
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
-            hintText: "البريد الالكتروني",
-          ),
-        ));
     final passwordField = Padding(
         padding: const EdgeInsets.only(right: 15.0, left: 15.0),
         child: TextFormField(
           obscureText: true,
           controller: passController,
           style: style,
+          validator: (val) => val.length < 8 ? 'كلمة المرور قصيرة' : null,
           keyboardType: TextInputType.text,
           textAlign: TextAlign.right,
           decoration: InputDecoration(
             contentPadding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
             hintText: "كلمة المرور",
+          ),
+        ));
+     final nameField = Padding(
+        padding: const EdgeInsets.only(right: 15.0, left: 15.0),
+        child: TextFormField(
+          obscureText: false,
+          controller: nameController,
+          style: style,
+          validator: (value) => value.isEmpty ? 'الرجاء ادخال الاسم':null,
+          keyboardType: TextInputType.text,
+          textAlign: TextAlign.right,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+            hintText: "الاسم الكامل",
+          ),
+        ));
+    final emailField = Padding(
+        padding: const EdgeInsets.only(right: 15.0, left: 15.0),
+        child: TextFormField(
+          obscureText: false,
+          controller: emailController,
+          style: style,
+          validator: (val) =>  !EmailValidator.validate(val, true)
+              ? 'الرجاء ادخال عنوان بريد صحيح'
+              : null,
+          keyboardType: TextInputType.emailAddress,
+          textAlign: TextAlign.right,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+            hintText: "البريد الالكتروني",
           ),
         ));
     final loginButon = Padding(
@@ -100,15 +102,19 @@ class _SignupState extends State<Signup> {
             minWidth: 150.0,
             padding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
             onPressed: () async {
-              Map map = {
-                'data': {
-                  'fullname': nameController.text,
-                  'email': emailController.text,
-                  'password': passController.text,
-                  'is_admin': '0'
-                }
-              };
-              createPost(CREATE_POST_URL, map);
+              if (_formKey.currentState.validate()) {
+                Map map = {
+                  'data': {
+                    'fullname': nameController.text,
+                    'email': emailController.text,
+                    'password': passController.text,
+                    'is_admin': '0'
+                  }
+                };
+                createPost(CREATE_POST_URL, map);
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text('Processing Data')));
+              }
             },
             child: Text("انشاء حساب",
                 textAlign: TextAlign.center,
@@ -116,61 +122,72 @@ class _SignupState extends State<Signup> {
           ),
         ));
 
-    return Scaffold(backgroundColor:Colors.white,
+    return Scaffold(
+        backgroundColor: Colors.white,
         body: new SingleChildScrollView(
-      child: Center(
-        child: Container(
-          color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                child: Image.asset(
-                  "assets/login.png",
-                  fit: BoxFit.fill,
-                  width:600.0 ,
-                ),
+          child: Center(
+            child: Container(
+              color: Colors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    child: Image.asset(
+                      "assets/login.png",
+                      fit: BoxFit.fill,
+                      width: 600.0,
+                    ),
+                  ),
+                  Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          nameField,
+                          SizedBox(height: 20.0),
+                          emailField,
+                          SizedBox(height: 20.0),
+                          passwordField,
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          loginButon,
+                          SizedBox(
+                            height: 25.0,
+                          ),
+                          new InkWell(
+                            child: new Text(
+                              "تسجيل دخول؟",
+                              style: TextStyle(
+                                  fontSize: 16.0, color: Colors.black45),
+                            ),
+                            onTap: () => Navigator.pop(
+                                  context,
+                                ),
+                          ),
+                        ],
+                      )),
+                  message == "signup"
+                      ? new Text(
+                          "تم التسجيل بنجاح",
+                          style:
+                              TextStyle(fontSize: 16.0, color: Colors.black45),
+                        )
+                      : message == "error"
+                          ? new Text(
+                              " خطأ في المعلومات",
+                              style: TextStyle(
+                                  fontSize: 16.0, color: Colors.black45),
+                            )
+                          : new Text(
+                              "",
+                              style: TextStyle(
+                                  fontSize: 16.0, color: Colors.black45),
+                            )
+                ],
               ),
-              nameField,
-              SizedBox(height: 20.0),
-              emailField,
-              SizedBox(height: 20.0),
-              passwordField,
-              SizedBox(
-                height: 20.0,
-              ),
-              loginButon,
-              SizedBox(
-                height: 25.0,
-              ),
-              new InkWell(
-                child: new Text(
-                  "تسجيل دخول؟",
-                  style: TextStyle(fontSize: 16.0, color: Colors.black45),
-                ),
-                onTap: () => Navigator.pop(
-                  context,
-                ),
-              ),
-              message == "signup"
-              ? new Text(
-                "تم التسجيل بنجاح",
-                style: TextStyle(fontSize: 16.0, color: Colors.black45),
-              )
-                  :  message == "error"
-        ? new Text(
-                " خطأ في المعلومات",
-                style: TextStyle(fontSize: 16.0, color: Colors.black45),
-              )
-                  :new Text(
-                "",
-                style: TextStyle(fontSize: 16.0, color: Colors.black45),
-              )
-            ],
+            ),
           ),
-        ),
-      ),
-    ));
+        ));
   }
 }
